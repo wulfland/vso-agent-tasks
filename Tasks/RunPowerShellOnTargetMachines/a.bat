cbf56199832d35695bcf30ba6de1744a99534a91
Muthukumaran Kasiviswanathan <muthuk@microsoft.com>
2015-04-16 18:21:35 +0530

    Adding parallel support in Copy & Deploy. Removing Alternate credentials support

---
+++ Tasks/RunPowerShellOnTargetMachines/a.bat
@@ -0,0 +1,11 @@
+	git add   ../../Tasks/CopyFilesToMachines/CopyFilesToMachines.ps1
+	git add   ../../Tasks/CopyFilesToMachines/task.json
+	git add   ../../Tasks/RunPowerShellOnTargetMachines/RunPowerShellOnTargetMachines.ps1
+	git add   ../../Tasks/RunPowerShellOnTargetMachines/task.json
+
+
+	git add ../../Tasks/CopyFilesToMachines/CopyJob.ps1
+	git add ../../Tasks/RunPowerShellOnTargetMachines/RunPowerShellJob.ps1
+	git add ../../Tasks/RunPowerShellOnTargetMachines/a.bat
+
+
